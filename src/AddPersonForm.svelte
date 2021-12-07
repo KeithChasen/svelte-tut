209af9f46b739057d488355294a93e2c51bc87cc
keithchasen <keithchasen89@gmail.com>
2021-12-07 16:06:44 +0200

    binding checked attribute of checkbox

--- src/AddPersonForm.svelte
+++ src/AddPersonForm.svelte
@@ -2,9 +2,12 @@
     let name;
     let color;
     let age;
+    let svelte = false;
+    let node = false;
+    let react = false;
 
     const handleSubmit = () => {
-        console.log(name, color, age)
+        console.log(name, color, age, svelte, node, react)
     }
 </script>
 
@@ -12,6 +15,10 @@
     <input type="text" placeholder="name" bind:value={name}>
     <input type="text" placeholder="color" bind:value={color}>
     <input type="number" placeholder="age" bind:value={age}>
+    <label>Skills:</label>
+    <input type="checkbox" bind:checked={svelte}>svelte<br />
+    <input type="checkbox" bind:checked={node}>node<br />
+    <input type="checkbox" bind:checked={react}>react<br />
     <button>Add</button>
 </form>
 
