06b819cff449097285c9d7b7f28108b884f02bb0
keithchasen <keithchasen89@gmail.com>
2021-12-07 15:57:32 +0200

    add person form component | preventDefault event modifier for form on:submit event

--- src/App.svelte
+++ src/App.svelte
@@ -1,5 +1,6 @@
 <script>
 	import Modal from "./Modal.svelte";
+	import AddPersonForm from "./AddPersonForm.svelte";
 
 	let showModal = false;
 
@@ -19,11 +20,7 @@
 </script>
 
 <Modal {showModal} on:click={toggleModal}>
-	<form>
-		<input type="text" placeholder="name">
-		<input type="text" placeholder="color">
-		<button>Add</button>
-	</form>
+	<AddPersonForm />
 	<h3 slot="title">Add a new person</h3>
 </Modal>
 <main>
