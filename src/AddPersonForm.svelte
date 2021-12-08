9b0a7d2e17340ec97acd6ce97366b72c6dee26ad
keithchasen <keithchasen89@gmail.com>
2021-12-08 14:42:02 +0200

    binding values for select

--- src/AddPersonForm.svelte
+++ src/AddPersonForm.svelte
@@ -11,12 +11,18 @@
 
 <form on:submit|preventDefault={handleSubmit}>
     <input type="text" placeholder="name" bind:value={name}>
-    <input type="text" placeholder="color" bind:value={color}>
     <input type="number" placeholder="age" bind:value={age}>
     <label>Skills:</label>
     <input type="checkbox" bind:group={skills} value="svelte">svelte<br />
     <input type="checkbox" bind:group={skills} value="node">node<br />
     <input type="checkbox" bind:group={skills} value="react">react<br />
+    <label>Color:</label>
+    <select bind:value={color}>
+        <option value="red">Red</option>
+        <option value="blue">Blue</option>
+        <option value="purple">Purple</option>
+        <option value="yellow">Yellow</option>
+    </select>
     <button>Add</button>
 </form>
 
